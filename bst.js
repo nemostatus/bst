1a72dc74d37478b0f218c773ac89a20b6a308e43
nemostatus <mattcorralejo1@gmail.com>
2021-03-23 03:58:02 -0700

    pseudocoding the insert method for the BST class

--- bst.js
+++ bst.js
@@ -8,6 +8,10 @@ class Node {
 
 class BST {
   constructor() {
-    this.root = null;
+    this.root = null
   }
+  //create a method to take in a value, insert it into the bst. To determine the location of the new val check if theres a
+  //root node. if theres no root node, then the newly inserted node is now the root node.
+  //if theres already a root node then use another method to determine where the node belongs
+  
 }
