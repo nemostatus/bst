2ce121fca4283dceedbbc4a8ef395daf9f4428c9
nemostatus <mattcorralejo1@gmail.com>
2021-03-23 03:20:52 -0700

    binary search tree  class created

--- bst.js
+++ bst.js
@@ -1,8 +1,13 @@
-class Node{
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right =null 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-    }
+class BST {
+  constructor() {
+    this.root = null;
+  }
 }
